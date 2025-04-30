cc892db43d6c4de21f6dbf1b415cc9f485719261
deepali.sohane <deepali.sohane@flick2know.com>
2025-05-01 01:10:20 +0530

    password hashing and login API

--- src/app.js
+++ src/app.js
@@ -1,27 +1,56 @@
 const express = require('express');
 const connectDb = require('./config/database');  
 const User = require('./models/user');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON request body it will be executed for every API call as we have not specified any route here
 
 app.post('/signup', async (req, res) => {
 
-    const user = new User(req.body); // Create a new user instance with the request body
-
     try{
-    await user.save().then(() => {
+        const {password} = req.body; // Destructure password from request body
+        const hashedPassword = await bcrypt.hash(password, 10); // Hash the password using bcrypt
+        req.body.password = hashedPassword; // Replace the password in the request body with the hashed password
+        const user = new User(req.body); // Create a new user instance with the request body
+        await user.save().then(() => {
         res.status(201).json({
             message: "User created successfully",
             user: user
         });
     })} catch (err){
         res.status(500).json({
-            message: "Error creating user",
+            message: "ERORR: Error creating user",
             error: err.message
         });
     };
 }); 
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body; // Destructure email and password from request body
+    try {
+        const user = await User.findOne({ email }); // Find the user by email in the database    
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password); // Compare the provided password with the hashed password in the database
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+        res.status(200).json({
+            message: "Login successful",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Error while logging in",
+            error: err.message
+        });
+    }           
+});
 
 app.get('/feed', async (req, res) => {
     try {
